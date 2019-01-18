abf6a22fcc493acebf82fb190330149ebc672562
Anton Kindestam <antonki@kth.se>
2019-01-18 18:41:58 +0100

    helpfix: add where-is command

--- helpfix.py
+++ helpfix.py
@@ -1,7 +1,9 @@
+import webmacs
 from webmacs.keymaps import keymap
 from webmacs.commands import define_command
 
 derp = __import__("webmacs.commands.global", fromlist=['']) # Don't name modules like keywords...
+
 @define_command("describe-key-briefly")
 def describe_binding_briefly(ctx):
     "Like emacs"
@@ -11,6 +13,28 @@ def describe_binding_briefly(ctx):
             "{key} runs the command {command} (keymap: {keymap})".format(**called_with)
         )
 
+
+class WhereIsCommandsListPrompt(derp.CommandsListPrompt):
+    label = "Where is command: "
+    history = webmacs.minibuffer.prompt.PromptHistory()
+
+
+@define_command("where-is")
+def where_is(ctx):
+    "Print short notice of where command is bound"
+    command = ctx.minibuffer.do_prompt(WhereIsCommandsListPrompt(ctx))
+    if not command:
+        return
+    command_bindings = list("{} (keymap: {})".format(k, kmapname)
+                            for kmapname, kmap in webmacs.keymaps.KEYMAPS.items()
+                            for k, v in kmap.all_bindings() if v == command)
+    if command_bindings:
+        ctx.minibuffer.show_info("{} is on: {}".format(command, ", ".join(command_bindings)))
+    else:
+        ctx.minibuffer.show_info("{} is not on any key".format(command))
+
+
 keymap("global").define_key("C-h c", "describe-key-briefly")
 keymap("global").define_key("C-h F", "describe-command")
 keymap("global").define_key("C-h f", "describe-command") # TODO: binding at "C-h f" that allows to list actual python functions (need a REPL first for this to make sense)
+keymap("global").define_key("C-h w", "where-is")
