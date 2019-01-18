47c159ace1f8c1946da571e2d10f8e9c39c60b26
Anton Kindestam <antonki@kth.se>
2019-01-18 17:17:40 +0100

    bindings: Bind C-x 5 2 and C-x 5 0 like in emacs

--- bindings.py
+++ bindings.py
@@ -2,3 +2,7 @@ from webmacs.keymaps import keymap
 
 keymap("global").define_key("C-x Right", "next-buffer")
 keymap("global").define_key("C-x Left", "previous-buffer")
+
+# # Like emacs or conkeror (upstream?)
+keymap("global").define_key("C-x 5 2", "make-window")
+keymap("global").define_key("C-x 5 0", "close-window")
