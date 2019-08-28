7ea975e2e66b6d7dcb5dadb5efb85a75e4d1b569
Anton Kindestam <antonki@kth.se>
2019-08-28 14:03:14 +0200

    bindings: bind Esc to buffer-escape

--- bindings.py
+++ bindings.py
@@ -7,3 +7,4 @@ keymap("global").define_key("C-x Left", "previous-buffer")
 keymap("global").define_key("C-x 5 2", "make-window")
 keymap("global").define_key("C-x 5 0", "close-window")
 keymap("global").define_key("C-x 5 o", "other-window")
+keymap("global").define_key("Esc", "buffer-escape") # So I can unfocus with escape, like in conkeror
